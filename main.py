4fc0f5d62270dc03bfb329d71e9845d2b9d6adbe
Reaperking <72340160+What1sLov3RK@users.noreply.github.com>
2021-11-19 16:56:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,16 +33,23 @@ async def command_start(message: types.Message):
     await state.set_state(States.all()[0])
 
 
-@dp.message_handler(commands=['search'])
+@dp.message_handler(commands=['search'],state=States.BASE)
 async def command_search(message: types.Message):
     state = dp.current_state(chat=message.chat.id, user=message.from_user.id)
     await bot.delete_message(message.chat.id, message.message_id)
+    await bot.send_message(message.from_user.id,"Send author name or song name ",reply_markup=nav.menu2)
     await state.set_state(States.all()[1])
 
 
 @dp.message_handler(state=States.MUSIC)
 async def download (message: types.Message):
+    print("boba")
     state = dp.current_state(chat=message.chat.id, user=message.from_user.id)
+    if message.text == '🔙':
+        await bot.delete_message(message.chat.id, message.message_id)
+        await bot.send_message(message.from_user.id, "Ok",reply_markup=nav.mainMenu)
+        await state.set_state(States.all()[0])
+        return
     html_content = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + urllib.parse.quote(message.text.replace(" ", "+"),encoding="utf8"))
     search_results = re.search(r"watch\?v=(\S{11})", html_content.read().decode())
     link = "https://www.youtube.com/" + str(search_results[0])
@@ -50,7 +57,6 @@ async def download (message: types.Message):
     path = YouTube(link).streams.filter(only_audio=True).first().download(output_path=os.getcwd() + r'\music')
     with open(path, "rb") as mp3:
         await bot.send_audio(message.from_user.id, mp3)
-    await state.set_state(States.all()[0])
 
 
 @dp.message_handler(state=States.BASE)
@@ -59,7 +65,7 @@ async def echo_send(message: types.Message):
         await bot.send_message(message.from_user.id,"https://www.youtube.com/watch?v=Q0EnwSTytE0")
     if message.text == '🔙':
         await bot.delete_message(message.chat.id, message.message_id)
-        await bot.send_message(message.from_user.id, "WIP",reply_markup=nav.mainMenu)
+        await bot.send_message(message.from_user.id, "Ok",reply_markup=nav.mainMenu)
 
 async def shutdown(dispatcher: Dispatcher):
     await dispatcher.storage.close()
